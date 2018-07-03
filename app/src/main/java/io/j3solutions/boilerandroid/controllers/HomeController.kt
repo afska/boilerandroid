ff7940fa55d41a82a395c4501b2e1b5b0a5dee07
Rodrigo Alfonso <rodri042@gmail.com>
2018-07-03 01:22:52 -0300

    Adding a simple go to with vertical animation

--- app/src/main/java/io/j3solutions/boilerandroid/controllers/HomeController.kt
+++ app/src/main/java/io/j3solutions/boilerandroid/controllers/HomeController.kt
@@ -2,9 +2,17 @@ package io.j3solutions.boilerandroid.controllers
 
 import android.view.View
 import io.j3solutions.boilerandroid.R
+import io.j3solutions.boilerandroid.views.changehandlers.VerticalChangeHandler
+import kotlinx.android.synthetic.main.controller_home.view.*
 
 class HomeController : BaseController(R.layout.controller_home) {
 	override fun onViewBound(view: View) {
-
+		view.goToPostsButton.setOnClickListener {
+			goTo(PostsController(), false) {
+				it
+					.popChangeHandler(VerticalChangeHandler())
+					.pushChangeHandler(VerticalChangeHandler())
+			}
+		}
 	}
 }
---
+++ app/src/main/java/io/j3solutions/boilerandroid/controllers/PostsController.kt
@@ -0,0 +1,5 @@
+package io.j3solutions.boilerandroid.controllers
+
+import io.j3solutions.boilerandroid.R
+
+class PostsController : BaseController(R.layout.controller_posts)

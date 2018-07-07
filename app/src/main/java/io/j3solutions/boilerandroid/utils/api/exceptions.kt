1b1fb274a1db52eb26107576a331fdf31f71c5cf
Rodrigo Alfonso <rodri042@gmail.com>
2018-07-07 05:20:56 -0300

    Adding error bodies

--- app/src/main/java/io/j3solutions/boilerandroid/utils/api/exceptions.kt
+++ app/src/main/java/io/j3solutions/boilerandroid/utils/api/exceptions.kt
@@ -2,4 +2,4 @@ package io.j3solutions.boilerandroid.utils.api
 
 import java.lang.RuntimeException
 
-class RequestFailedException(val statusCode: Int) : RuntimeException()
+class RequestFailedException(val statusCode: Int, body: Map<String, String>) : RuntimeException()

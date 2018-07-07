1cbda9d5e230af3a13975216a19149a5f02514c3
Rodrigo Alfonso <rodri042@gmail.com>
2018-07-07 05:53:09 -0300

    Updating json to JsonObject

--- app/src/main/java/io/j3solutions/boilerandroid/utils/api/exceptions.kt
+++ app/src/main/java/io/j3solutions/boilerandroid/utils/api/exceptions.kt
@@ -1,5 +1,6 @@
-package io.j3solutions.boilerandroid.utils.api
+package io.j3solutions.suterhandroid.utils.api
 
+import com.google.gson.JsonObject
 import java.lang.RuntimeException
 
-class RequestFailedException(val statusCode: Int, val body: Map<String, String>?) : RuntimeException()
+class RequestFailedException(val statusCode: Int, val body: JsonObject?) : RuntimeException()

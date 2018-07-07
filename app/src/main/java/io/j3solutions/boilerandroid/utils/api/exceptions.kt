5be46311fe737719e65174d319d4a691028f9ee7
Rodrigo Alfonso <rodri042@gmail.com>
2018-07-07 05:22:52 -0300

    This is public

--- app/src/main/java/io/j3solutions/boilerandroid/utils/api/exceptions.kt
+++ app/src/main/java/io/j3solutions/boilerandroid/utils/api/exceptions.kt
@@ -2,4 +2,4 @@ package io.j3solutions.boilerandroid.utils.api
 
 import java.lang.RuntimeException
 
-class RequestFailedException(val statusCode: Int, body: Map<String, String>?) : RuntimeException()
+class RequestFailedException(val statusCode: Int, val body: Map<String, String>?) : RuntimeException()

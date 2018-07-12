68aa2c284a8e31c1b9440ac7ff0f7b3ff5b5fdab
Rodrigo Alfonso <rodri042@gmail.com>
2018-07-12 06:32:33 -0300

    Adding SimplePagerAdapter

---
+++ app/src/main/java/io/j3solutions/boilerandroid/views/adapters/SimplePagerAdapter.kt
@@ -0,0 +1,51 @@
+package io.j3solutions.boilerandroid.views.adapters
+
+import android.content.Context
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SimplePagerAdapter<T>(
+	private val context: Context?,
+	private val layout: Int,
+	private val itemSetter: (View, T, SimplePagerAdapter<T>) -> Unit,
+	private val pageWidth: Float = 1f
+) : PagerAdapter() {
+	var items: List<T> = listOf()
+
+	fun populate(items: List<T>) {
+		this.items = items
+		notifyDataSetChanged()
+	}
+
+	override fun instantiateItem(container: ViewGroup, position: Int): Any {
+		val inflater = LayoutInflater.from(context)
+		val layout = inflater.inflate(layout, container, false) as ViewGroup
+
+		itemSetter(layout, items.get(position), this)
+
+		container.addView(layout)
+		return layout
+	}
+
+	override fun destroyItem(container: ViewGroup, position: Int, view: Any) {
+		container.removeView(view as View)
+	}
+
+	override fun getCount(): Int {
+		return items.size
+	}
+
+	override fun isViewFromObject(view: View, obj: Any): Boolean {
+		return view == obj
+	}
+
+	override fun getPageWidth(position: Int): Float {
+		return pageWidth
+	}
+
+	override fun getPageTitle(position: Int): CharSequence? {
+		return ""
+	}
+}

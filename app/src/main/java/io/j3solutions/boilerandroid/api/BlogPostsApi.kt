e32838823b678c47b66f47cf9fb97ef65d65e610
Rodrigo Alfonso <rodri042@gmail.com>
2019-05-07 15:59:31 -0300

    Updating blogs url

--- app/src/main/java/io/j3solutions/boilerandroid/api/BlogPostsApi.kt
+++ app/src/main/java/io/j3solutions/boilerandroid/api/BlogPostsApi.kt
@@ -7,10 +7,10 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface BlogPostsApi {
-	@GET("posts")
+	@GET("boilerandroid.json")
 	fun getAll(): Single<Response<List<BlogPost>>>
 
 	companion object {
-		val api: BlogPostsApi get() = newApi("https://boilerandroid.000webhostapp.com/wp-json/wp/v2/")
+		val api: BlogPostsApi get() = newApi("https://gist.githubusercontent.com/rodri042/6e2d1ac8c8a9ecfa6cd10bbed4ac9389/raw/dd49dd24ab7f03fc1da2edc330dc34b54722f693/")
 	}
 }
